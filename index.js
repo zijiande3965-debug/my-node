6e766b102c82bd98954188762a5434fd3e3caf55
王子健 <wangzijian@zzschj.com>
2026-09-02 17:57:37 +0800

    feat: 新增 /users 接口

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello World! 我的第一个 Node.js 接口跑起来了');
 });
 
+app.get('/users', (req, res) => {
+  res.send('Hello Wangzijian! 我的第一个 Node.js 接口跑起来了');
+});
+
 app.listen(PORT, () => {
   console.log(`服务已启动，访问 http://localhost:${PORT}`);
 });
